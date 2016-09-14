92bf2a7991c8806d094638cc9e1ab0714131f33f
zhenggf <zheng.guifu@foxmail.com>
2016-09-14 17:42:16 +0800

    123

---
+++ config/src/main/java/cn/orgid/funny/config/domain/component/SystemConfig.java
@@ -0,0 +1,17 @@
+package cn.orgid.funny.config.domain.component;
+
+public class SystemConfig {
+	
+	private String encryptKey;
+
+	public String getEncriptKey() {
+		return encryptKey;
+	}
+
+	public void setEncriptKey(String encriptKey) {
+		this.encryptKey = encriptKey;
+	}
+	
+	
+
+}
