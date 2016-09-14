af3b8ff48132f915e3bf006c087e6e1de8b6c3eb
zhenggf <zheng.guifu@foxmail.com>
2016-09-14 22:24:42 +0800

    1

--- config/src/main/java/cn/orgid/funny/config/domain/service/DeveloperService.java
+++ config/src/main/java/cn/orgid/funny/config/domain/service/DeveloperService.java
@@ -1,5 +1,18 @@
 package cn.orgid.funny.config.domain.service;
 
+import org.springframework.stereotype.Service;
+
+import cn.orgid.funny.config.domain.model.dev.Developer;
+
+@Service
 public class DeveloperService {
+	
+	
+	
+	public void register(Developer developer){
+		
+		
+		
+	}
 
 }
